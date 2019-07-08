4fd9581caa579b0c685c001b2de2d98d2a3de770
OrzPen <529234641@qq.com>
2019-07-08 19:18:17 +0800

    删除功能

--- src/views/heros/AppList.vue
+++ src/views/heros/AppList.vue
@@ -23,9 +23,7 @@
                 <td>
                   <a href="edit.html">edit</a>
                   &nbsp;&nbsp;
-                  <a
-                    href="javascript:window.confirm('Are you sure?')"
-                  >delete</a>
+                  <a href="javascript:" @click="del(item.id)">删除</a>
                 </td>
               </tr>
             </tbody>
@@ -38,41 +36,60 @@
 
 <script>
 //导入axios
-import axios from 'axios'
+import axios from "axios";
 export default {
   //定义英雄列表页数据
   data() {
     return {
-      list : []
-    }
+      list: []
+    };
   },
   //在组件挂载完毕时发送ajax
   mounted() {
-    this.getHeros()
+    this.getHeros();
   },
   methods: {
-    getHeros () {
+    getHeros() {
       axios
         //开启json-server服务器,虚拟数据服务器
-        .get ('http://localhost:3000/heroes')
-        .then ((res) => {
+        .get("http://localhost:3000/heroes")
+        .then(res => {
           //解构res
-          const {data , status} = res
+          const { data, status } = res;
           //判断获取成功和失败
           if (status == 200) {
             //把获取到的数据添加到实例中准备好的空数组中
-            this.list = data
+            this.list = data;
             // console.log (this.list)
           }
-
         })
         //获取失败
-        .catch ((err) => {
-          alert ('服务器异常')
+        .catch(err => {
+          alert("服务器异常" + err);
           // console.log (err)
+        });
+    },
+    del(id) {
+      //确认是否要删除
+      if (!confirm("确认删除?")) {
+        return false;
+      }
+      axios
+        .delete(`http://localhost:3000/heroes/${id}`)
+        .then(res => {
+          //判断删除成功和失败
+          if (res.status == 200) {
+            //如果成功,重新加载页面
+            this.getHeros();
+          }
         })
+        //获取失败
+        .catch(err => {
+          alert("服务器异常" + err);
+          // console.log (err)
+        });
     }
-  },
+  }
 };
 </script>
 
