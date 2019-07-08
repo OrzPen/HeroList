5389aa9a1a72ef646576d79ed25befff1c1c59b4
OrzPen <529234641@qq.com>
2019-07-08 19:45:06 +0800

    实现添加功能

--- src/views/heros/AddHeroes.vue
+++ src/views/heros/AddHeroes.vue
@@ -4,23 +4,48 @@
         <form>
           <div class="form-group">
             <label for="txtName">姓名</label>
-            <input type="text" class="form-control" id="txtName" placeholder="英雄姓名">
+            <input type="text" class="form-control" id="txtName" v-model="formData.name" placeholder="英雄姓名">
           </div>
           <div class="form-group">
             <label for="sex">性别</label>
-            <select class="form-control">
+            <select class="form-control" v-model="formData.gender">
                 <option value="男">男</option>
                 <option value="女">女</option>
             </select>
           </div>
-          <button type="submit" class="btn btn-success">提交</button>
+          <button type="submit" @click.prevent="add" class="btn btn-success">提交</button>
         </form>
       </div>
 </template>
 
 <script>
+import axios from 'axios' 
 export default {
-
+  data() {
+    return {
+      formData : {
+        name : '',
+        gender : "男"
+      }
+    }
+  },
+  methods : {
+    add() {
+      axios.post ("http://localhost:3000/heroes" , this.formData)
+      .then((res) => {
+          //判断删除成功和失败
+          if (res.status == 201) {
+            //如果成功,跳转回英雄列表页
+            this.$router.push ({name : 'heroes'})
+          }
+        })
+        //获取失败
+        .catch(err => {
+          alert("服务器异常" + err);
+          // console.log (err)
+        });
+    }
+  }
 }
 </script>
 
